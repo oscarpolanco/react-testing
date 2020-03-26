9baff507ab922a0c0fc05dda501a7323b3b17d26
Oscar Polanco <opolanco@intellisys.com.do>
2020-03-26 18:15:38 -0400

    Let jest know when is targeting a test file and fix an issue on the babelrc file

--- section_1_to_xx/section_2_configuring_jest_babel/calculator/.babelrc.js
+++ section_1_to_xx/section_2_configuring_jest_babel/calculator/.babelrc.js
@@ -1,9 +1,11 @@
+const isTest = String(process.env.NODE_ENV) === 'test';
+
 module.exports = {
-  presets: [['@babel/env', {modules: false}], '@babel/react'],
+  presets: [['@babel/env', {modules: isTest ? 'commonjs' : false}], '@babel/react'],
   plugins: [
     'syntax-dynamic-import',
     'transform-class-properties',
-    'transform-object-rest-spread',
+    '@babel/plugin-proposal-object-rest-spread',
   ],
 }
 /*
