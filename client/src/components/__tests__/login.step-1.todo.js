09ea6c214201aa62b9679460ce0e6873dcccf305
Oscar Polanco <opolanco@intellisys.com.do>
2020-03-31 17:05:27 -0400

    Add test for the login form

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from '../login';
+
 // Basic unit test
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
@@ -17,19 +21,21 @@ test('calls onSubmit with the username and password when submitted', () => {
   //
   // Assert
   // ensure your submit handler was called properly
-})
+  const container = document.createElement('div');
+  const handleSubmit = jest.fn();
+  ReactDOM.render(<Login onSubmit={handleSubmit} />, container);
+  const form = container.querySelector('form');
+  const { username, password } = form.elements;
+  username.value = 'foo';
+  password.value = 'test';
 
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=Testing&e=login.step-1&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
-////////////////////////////////
+  form.dispatchEvent(new window.Event('submit'));
+
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username: username.value,
+    password: password.value
+  });
+
+
+});
