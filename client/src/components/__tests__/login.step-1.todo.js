1b866bd9556f643e75eb1b6b629ef7bd0fb4116a
Oscar Polanco <opolanco@intellisys.com.do>
2020-04-02 15:17:32 -0400

    Add a snapshot test

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import {generate} from 'til-client-test-utils';
-import {cleanup, renderIntoDocument} from 'react-testing-library';
+import {cleanup, renderIntoDocument, render} from 'react-testing-library';
 import Login from '../login';
-
-// Due to the fact that our element is not in the document, the
-// click event on the submit button will not be treated as a
-// submit event on the form which is why we're simulating a submit
-// event on the form rather than clicking the button and then
-// asserting the button's type is set to submit rather than just
-// clicking on the button.
-//
-// Alternatively, we could actually insert the element directly into
-// the document, then click on the button and that should work!
-// Try doing that!
-// (Tip: document.body.appendChild(container) and getByText('submit').click())
-//
-// Bonus: Don't forget to cleanup after yourselve when you're finished so you don't
-// have things hanging out in the document!
-//
-// Extra bonus, rather than manually inserting the container into the document
-// check out the docs for react-testing-library and the renderIntoDocument method!
 
 afterEach(cleanup);
 
@@ -41,4 +23,11 @@ test('calls onSubmit with the username and password when submitted', () => {
   // Assert
   expect(handleSubmit).toHaveBeenCalledTimes(1);
   expect(handleSubmit).toHaveBeenCalledWith(fakeUser);
+});
+
+test('snapshot', () => {
+  // render the login, this will give you back an object with a `container` property
+  // expect the `container` property to match a snapshot
+  const { container } = render(<Login />);
+  expect(container).toMatchSnapshot();
 });
