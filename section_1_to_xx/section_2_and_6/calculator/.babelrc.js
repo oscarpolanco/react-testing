87e7a36aec9cf8efbd5ae673ba3573558c17458e
Oscar Polanco <opolanco@intellisys.com.do>
2020-04-06 18:46:54 -0400

    Add cypress and scripts to run it

--- section_1_to_xx/section_2_and_6/calculator/.babelrc.js
+++ section_1_to_xx/section_2_and_6/calculator/.babelrc.js
@@ -1,86 +1,10 @@
-const isTest = String(process.env.NODE_ENV) === 'test';
-
+const isTest = String(process.env.NODE_ENV) === 'test'
 module.exports = {
-  presets: [['@babel/env', {modules: isTest ? 'commonjs' : false}], '@babel/react'],
+  presets: [['env', {modules: isTest ? 'commonjs' : false}], 'react'],
   plugins: [
     'syntax-dynamic-import',
     'transform-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    isTest ? 'dynamic-import-node': null
+    'transform-object-rest-spread',
+    isTest ? 'dynamic-import-node' : null,
   ].filter(Boolean),
 }
-/*
-Solution snippets below
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const isTest = String(process.env.NODE_ENV) === 'test'
-
-
-for the env plugin modules config:
-isTest ? 'commonjs' : false
-
-For dynamic import config in plugins
-isTest ? 'dynamic-import-node' : null
- */
